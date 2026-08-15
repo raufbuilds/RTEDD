775614be9c66dbb11c89075b62b675718119c81b
Abdur Rauf <a.raufoffice@gmail.com>
2026-08-15 21:10:53 +0600

    Update start_laptop2.bat

--- dashboard/start_laptop2.bat
+++ dashboard/start_laptop2.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Move to the root directory where the .bat file sits
+:: Move to the dashboard directory where this .bat file sits
 cd /D "%~dp0"
 
 :: Use quotes around the path to handle the space in "IT BD"
@@ -12,5 +12,5 @@ if exist "..\venv\Scripts\activate.bat" (
 
 echo Starting dashboard...
 :: Use 'python -m' as a backup way to trigger streamlit
-python -m streamlit run dashboard.py
+python -m streamlit run dashboard.py --server.address 127.0.0.1
 pause
